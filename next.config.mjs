67c4a6020186cc97fa9dea7dabfe2166db80214c
momadAB <mabaqer99@gmail.com>
2024-10-28 09:30:33 +0300

    Homepage added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "boubyan.bankboubyan.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
