7483aebd8b75a0fffc45f6559bf7bdaee9a9404a
momadAB <mabaqer99@gmail.com>
2024-10-30 11:43:20 +0300

    Added search bar for users

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -33,12 +33,24 @@ function UserList({ users }) {
     <div className="bg-white-900 min-h-screen h-screen flex items-center justify-center  inset-0 z-[-1] ">
       <div className="max-w-[90%] overflow-scroll w-full px-6 py-8 bg-white-800 rounded-md shadow-md hover:shadow-lg max-h-[80%] ">
         <h2 className="text-3xl text-black font-semibold mb-6">Users</h2>
+
+        {/* Search Input */}
+        <input
+          type="text"
+          placeholder="Search by username"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full p-2 mb-6 border border-gray-300 rounded-md"
+        />
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {loading
             ? Array.from({ length: 6 }).map((_, index) => (
                 <UserCardLoader key={index} />
               ))
-            : users.map((user) => <IndividualUserCard user={user} />)}
+            : filteredUsers.map((user) => (
+                <IndividualUserCard key={user._id} user={user} />
+              ))}
         </div>
       </div>
     </div>
