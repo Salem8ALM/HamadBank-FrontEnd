8b09b314b8df9fd8697331c4132da03fc028b0c3
alhajerih <alhajerih@hotmail.com>
2024-10-30 21:43:03 +0300

    edit

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,8 +1,55 @@
+"use client";
 import UserProfile from "@/components/UserProfile";
 import React from "react";
-
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 function ProfilePage() {
-  return <UserProfile />;
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+  return (
+    <div>
+      <UserProfile />
+      <div>
+        <Button onClick={handleClick}>Open Snackbar</Button>
+        <Snackbar
+          open={open}
+          autoHideDuration={1000}
+          onClose={handleClose}
+          message="Note archived"
+          action={action}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ProfilePage;
